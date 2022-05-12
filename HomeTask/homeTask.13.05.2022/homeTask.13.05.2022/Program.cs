1aaec0c68526398978895c7bfd877ac39333855e
khlzemil <emilmg@code.edu.az>
2022-05-13 01:23:55 +0400

    Added hometask

---
+++ HomeTask/homeTask.13.05.2022/homeTask.13.05.2022/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class Program
+
+{
+    
+    public static void Main()
+    {
+         string number = Console.ReadLine();
+        int n = int.Parse(number);
+
+        if (IsPrime(n))
+            Console.WriteLine("True");
+        else
+            Console.WriteLine("False");
+        
+    }
+        public static bool IsPrime(int num)
+        {
+            for (int i = 2; i < num; i++)
+                if (num % i == 0)
+                    return false;
+            return true;
+        }
+}
+
+
---
+++ HomeTask/hometask2.13.05.2022/hometask2.13.05.2022/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Program
+{
+
+    public static void Main()
+    {
+        int[] StudentPoint = { 30, 35, 55, 65, 90, 85 };
+        CalcAvg(StudentPoint);
+    }
+
+    public static void CalcAvg(int[] StudentPoint)
+    {
+        int counter = 0;
+        float sum = 0;
+        float avaragePoint = 0;
+        foreach(int element in StudentPoint)
+        {
+            sum += element;
+            counter++;
+        }
+        avaragePoint = sum / counter;
+        Console.WriteLine(avaragePoint);
+        if (avaragePoint > 60)
+            Console.WriteLine("Məzun oldunuz");
+        else
+            Console.WriteLine("Məzun ola bilmədiniz");
+
+    }
+
+}
